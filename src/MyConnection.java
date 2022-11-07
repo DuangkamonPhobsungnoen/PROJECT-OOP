93e6ec356ddba53dc56c32b6e70e79d52b1acfec
Dnuangkamon <88492141+Dnuangkamon@users.noreply.github.com>
2022-11-07 23:55:40 +0700

    Login

---
+++ src/MyConnection.java
@@ -0,0 +1,20 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class MyConnection {
+    
+    // create a function to connect with mysql database
+    public static Connection getConnection(){
+     
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/register", "root", "");
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        return con;
+    }
+    
+}    

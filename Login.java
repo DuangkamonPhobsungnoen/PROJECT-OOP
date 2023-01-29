1e53fc60151a7d9be10a0a7ba375d7caf4cb5680
Dnuangkamon <88492141+DuangkamonPhobsungnoen@users.noreply.github.com>
2023-01-29 22:53:52 +0700

    push ใหม่

--- Login.java
+++
@@ -1,8 +0,0 @@
-
-import java.io.Serializable;
-public class Login implements Serializable{
-    private static final long SerialVersionUID = 1l;
-    public String username;
-   public String password;
-    
-}
--- UserModel.java
+++
@@ -1,6 +0,0 @@
-
-public class UserModel {
-    public void showBill(UserView view, String text) {
-        view.getJtabill().setText(text);
-    }
-}
